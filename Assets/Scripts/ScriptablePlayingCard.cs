2fc4643e4ed7c8c210e22f11e0fd7f29b144dc02
Alex McDowell <alexmcdhs@gmail.com>
2024-12-17 22:16:42 -0600

    Creating base layout and template for other scenes

---
+++ Assets/Scripts/ScriptablePlayingCard.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptablePlayingCard", menuName = "Scriptable Objects/PlayingCard")]
+public class ScriptabledPlayingCard : ScriptableObject
+{
+    public new string name;
+    public int numericalValue;
+    public Sprite cardImage;
+
+    //Suit Values
+    //1 Clubs|2 Diamonds|3 Hearts|4 Spades
+    public int suit;
+
+    public int drawChance;
+}
